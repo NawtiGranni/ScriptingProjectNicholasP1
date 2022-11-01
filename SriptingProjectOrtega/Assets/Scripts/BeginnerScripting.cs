508ebd5b8c2a38bdf2bc750d22d042ba057aedaa
NawtiGranni <Nickyy2006@outlook.com>
2022-11-01 09:56:56 -0700

    added scripts
    I added more scrpts to the script project

--- SriptingProjectOrtega/Assets/Scripts/BeginnerScripting.cs
+++ SriptingProjectOrtega/Assets/Scripts/BeginnerScripting.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class BeginnerScripting : MonoBehaviour
 {
-    float coffeeTemprature = 85.0f;
-    float hotLimitTempature = 70.0f;
-    float coldLimitTemperature = 40.0f
+    float coffeeTemperature = 85.0f;
+    float hotLimitTemperature = 70.0f;
+    float coldLimitTemperature = 40.0f;
 
 
-    
-    void Update ()
+
+
+    void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
             TemperatureTest();
@@ -22,21 +23,23 @@ public class BeginnerScripting : MonoBehaviour
     void TemperatureTest()
     {
         // If theh coffee's temperature is greater than the hottest drinking temperature...
-        if(coffeeTemperature > hotLimitTemperature)
+        if (coffeeTemperature > hotLimitTemperature)
         {
             // ... do this.
             print("Coffee is too hot.");
         }
         // If it isn't, but the coffee temperature is less than the coldest drinking temperature...
-        else if(coffeeTemperature < coldLimitTemperature)
-    {
+        else if (coffeeTemperature < coldLimitTemperature)
+        {
             // ...do this.
             print("Coffee is too cold.");
-    }
+        }
         // If it is neither of those then...
         else
         {
             // ... do this.
             print("Coffee is just right.");
         }
+     }
 }
+    
